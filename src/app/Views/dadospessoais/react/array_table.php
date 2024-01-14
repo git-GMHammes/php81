bae5d25d79b9a49e972e1052e343c04ddddf68f1
Gustavo Hammes <sistema.gustavo@gmail.com>
2024-01-14 18:57:36 -0300

    UP

--- src/app/Views/dadospessoais/react/array_table.php
+++ src/app/Views/dadospessoais/react/array_table.php
@@ -1,3 +1,7 @@
+<div id="reactApp" data-results='<?php echo json_encode($result); ?>'></div>
+
+
+
 <?php
 myPrint($result, 'src\app\Views\dadospessoais\react\array_table.php');
 ?>
--- src/app/Views/dadospessoais/react/copy.php
+++ src/app/Views/dadospessoais/react/copy.php
@@ -1,32 +1,45 @@
 <!-- Exemplo de View em CodeIgniter 4 -->
-<div id="reactApp" data-results='<?php echo json_encode($result); ?>'></div>
+<div id="reactApp" data-result='<?php echo json_encode($result); ?>'></div>
 
+<!-- Inclusão do React e ReactDOM via CDN -->
 <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
 <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
 
 <script>
+    // Importando os hooks 'useEffect' e 'useState' do objeto React
     const { useEffect, useState } = React;
 
+    // Definindo um componente funcional React chamado 'MyComponent'
     function MyComponent() {
+        // Inicializando o estado 'data' com um array vazio
+        // 'setData' é a função que será usada para atualizar 'data'
         const [data, setData] = useState([]);
 
+        // O Hook useEffect é usado para lidar com efeitos colaterais
+        // Aqui, é usado para carregar dados quando o componente é montado
         useEffect(() => {
-            // Lê os dados do elemento DOM
+            // Acessando o elemento DOM com id 'reactApp'
             const element = document.getElementById('reactApp');
-            const results = JSON.parse(element.getAttribute('data-results'));
+            // Lendo o atributo 'data-result', que contém os dados em formato JSON
+            const result = JSON.parse(element.getAttribute('data-result'));
 
-            setData(results);
-        }, []);
+            // Atualizando o estado 'data' com os dados convertidos
+            setData(result);
+        }, []); // O array vazio indica que o efeito roda apenas na montagem do componente
 
+        // Renderização do componente: mapeando cada item de 'data' para um div
         return (
             <div>
                 {data.map((item, index) => (
+                    // Para cada item de 'data', criamos um div com o conteúdo sendo a stringificação do item
+                    // 'key' é necessário em listas para ajudar o React a identificar quais itens mudaram
                     <div key={index}>{JSON.stringify(item)}</div>
                 ))}
             </div>
         );
     }
 
-    // Monta o componente React no elemento com id 'reactApp'
+    // Renderizando 'MyComponent' dentro do elemento DOM com id 'reactApp'
     ReactDOM.render(<MyComponent />, document.getElementById('reactApp'));
 </script>
+
