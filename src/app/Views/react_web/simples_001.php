da0d6a84cfc2330176e65b41959f47f7f4ccc390
Gustavo Hammes <sistema.gustavo@gmail.com>
2024-03-09 20:16:09 -0300

    UP

--- src/app/Views/react_web/simples_001.php
+++ src/app/Views/react_web/simples_001.php
@@ -2,14 +2,16 @@
 <script type="text/babel">
     function AppCard() {
         return (
+            <div>
             <div class="container">
-                <div className="card mb-4 mt-4 ms-4 me-4" style={{width: '18rem'}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Crad em Bootstrap</h5>
-                        <p className="card-text">Conteúdo principal do card. Aqui vai o corpo do card, onde você pode colocar qualquer informação.</p>
-                        <a href="#" className="btn btn-primary">Rodapé do Card</a>
-                    </div>
-                </div>
+            <div className="card mb-4 mt-4 ms-4 me-4" style={{width: '18rem'}}>
+            <div className="card-body">
+            <h5 className="card-title">Crad em Bootstrap</h5>
+            <p className="card-text">Conteúdo principal do card. Aqui vai o corpo do card, onde você pode colocar qualquer informação.</p>
+            <a href="#" className="btn btn-primary">Rodapé do Card</a>
+            </div>
+            </div>
+            </div>
             </div>
         );
     }
